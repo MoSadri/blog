04a12d670f6d613bcb4a6007c75146c810b96f37
MohSadri <mohxsadri@gmail.com>
2021-04-26 02:22:02 +0430

    initial commit

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,14 @@
 from django.db import models #importing class model
+from django.urls import reverse #new
 
 
-class Post(models.Model): #subclass of models 
+class Post(models.Model): #subclass of models
     title = models.CharField(max_length=200)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE,)
     body = models.TextField()
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self): # new
+        return reverse('post_detail', args=[str(self.id)])
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView #new
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
+
 
 
 class BlogListView(ListView):
@@ -12,4 +15,17 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-    
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields= ['title','author','body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView): #new
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
