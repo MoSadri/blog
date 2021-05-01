a1d0ed1ed68a4a10d53d538948f83286eb0ecd25
MohSadri <mohxsadri@gmail.com>
2021-05-01 17:26:17 +0430

    forms and user accounts

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')), # new]
+    path('accounts/', include('django.contrib.auth.urls')), #new
+    path('accounts/', include('accounts.urls')), #new
+    path('', include('blog.urls')),
 ]
